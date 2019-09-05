16c4b31269437d4a8caeb5c89d071d681b7eb5f8
ahustis20 <ahustis20@gmail.com>
2019-09-05 12:34:07 -0600

    Update and rename proj6/WordSortDemo.java to QuickSortProject/WordSortDemo.java

--- QuickSortProject/WordSortDemo.java
+++ QuickSortProject/WordSortDemo.java
@@ -1,34 +1,4 @@
-package proj6;
-
-/** 
- * Project 6
- * 
- * Use Quicksort or Mergesort (your choice) to sort words from a file
- * by word length, from the longest words to the shortest.
- * You may use the implementation given in the book for Mergesort and/or Quicksort.
- * 
- * Write the output of the program to files Lexicographical.txt and TaleOfTwoCitiesLongToShort.txt.
- * Submit four files for grading:
- * 1. the sorted text file
- * 2. the modified WordSorter Demo program
- * 3. your Quicksort or Mergesort implementation, and
- * 4. the StringLengthComparitor.
- * 
- * Included in this week's content are
- * 1. IOExample.java -- an example program showing how read from and write to files Java 
- * 2. SortExample.java -- an example program demonstrating the use of comparators
- * 3. TaleOfTwoCities.txt -- a text file of Charles Dickens' masterpiece opening to the book
- * 4. WordSortDemo driver file to get you started
- *     
- *     Note: Punctuation marks will be stuck to some of the words. That's fine.
- *     
- *  Grading:
- *  3 pt - Your implementation of Mergesort or Quicksort
- *  2 pt - Your implementation of StringLengthComparitor
- *  1 pt - The text file sorted from longest to shorts word
- *  3 pt - Your modified WordSortDemo program putting it all together
- *  1 pt - Proper use of Javadoc comments and coherent code
- */
+package QuickSortProject;
 
 import java.util.ArrayList;
 import java.util.Comparator;
